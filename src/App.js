67d3433071775453427cfc09a07b4eb513728d0f
luiscaputo <luiscaputo15@gmail.com>
2021-07-07 23:04:31 +0100

    :construction: constructing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {react} from 'react';
+import {Home} from './pages/home/home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+            <Route path="/" component= {Home}></Route>
+          </Switch>
+        </Router>
       </header>
+      <body>
+        <h1>teste</h1>
+      </body>
     </div>
   );
 }
---
+++ src/pages/home/home.js
@@ -0,0 +1,27 @@
+import {React} from 'react';
+import '../../App.css';
+
+
+
+export const Home = () =>{
+  return(
+    <div>
+      <h4>Sistema de Gestão Financeira</h4>
+      {/* <ul className="list">
+        <li className="items-list">
+          <a className="link-list"
+          href=""
+          >
+            Pagina Inicial
+          </a>
+        </li>
+        <li>
+          Home
+        </li>
+        <li>
+          Home
+        </li>
+      </ul> */}
+    </div>
+  )
+}
